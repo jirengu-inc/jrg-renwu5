5898b2be1c3f85bea6e575702442adf7d4c5f947
wcongratulation <1294687902@qq.com>
2017-03-11 23:46:49 +0800

    ss

--- 钱天鸿/高级4-requirejs/js/app/index.js
+++
@@ -1,6 +0,0 @@
-
-define(['jquery','com/carousel','com/waterfall_ajax','com/gotop'],function($,carousel,waterfall,gotop){
-	carousel.init($('.carousel'));
-	new waterfall($('.pic-ct'));
-	new gotop();
-})
