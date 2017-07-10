d37a3ca8f7f869ab3ac23f2a76f90f67e4c511e9
siminmin <simin_jun@163.com>
2017-07-10 16:52:17 +0800

    add my files

---
+++ 吴思敏/js_task_15/.router.js
@@ -0,0 +1,15 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/getMore', function(req, res){
+
+    var len=req.query.len;
+    var index=req.query.index;
+    var loadMore=index*len;
+    var arr=[loadMore,loadMore+1,loadMore+2];
+    res.send({
+    	status:0,
+    	arr
+    })
+})}
+ module.exports.setRouter = setRouter
